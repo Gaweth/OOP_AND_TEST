fafdb48baa93fc7fbff6e501bbfc5a2f556dedb8
Kuba <mcdon1@wp.pl>
2021-01-16 10:48:10 +0100

    First

---
+++ src/main/java/org/example/OOP_basic/abstractionAndPoli/Animals.java
@@ -0,0 +1,10 @@
+package org.example.OOP_basic.abstractionAndPoli;
+
+public class Animals {
+
+    public String Name;
+
+     public void move(){
+
+    }
+}
---
+++ src/main/java/org/example/OOP_basic/abstractionAndPoli/Worm.java
@@ -0,0 +1,7 @@
+package org.example.OOP_basic.abstractionAndPoli;
+
+public abstract class Worm extends Animals{
+
+    public abstract void hibernate();
+
+}
